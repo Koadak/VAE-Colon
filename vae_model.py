eb484dc44d9e408c870ce0f4a6dad4b08b99060b
Koadak <jdg5959@psu.edu>
2025-03-19 23:31:23 -0400

    First commit

---
+++ vae_model.py
@@ -0,0 +1,102 @@
+import tensorflow as tf
+from tensorflow.keras.layers import Input, Conv2D, Flatten, Dense, Reshape, Conv2DTranspose, Lambda
+from tensorflow.keras.models import Model
+import tensorflow.keras.backend as K
+
+IMG_SHAPE = (224, 224, 3)
+LATENT_DIM = 128  # Dimensionality of the latent space
+
+# Encoder
+def build_encoder():
+    inputs = Input(shape=IMG_SHAPE)
+
+    x = Conv2D(32, (3, 3), activation='relu', strides=2, padding='same')(inputs)
+    x = Conv2D(64, (3, 3), activation='relu', strides=2, padding='same')(x)
+    x = Conv2D(128, (3, 3), activation='relu', strides=2, padding='same')(x)
+    x = Flatten()(x)
+
+    z_mean = Dense(LATENT_DIM, name="z_mean")(x)
+    z_log_var = Dense(LATENT_DIM, name="z_log_var")(x)
+
+    def sampling(args):
+        z_mean, z_log_var = args
+        epsilon = K.random_normal(shape=(K.shape(z_mean)[0], LATENT_DIM), mean=0., stddev=1.0)
+        return z_mean + K.exp(0.5 * z_log_var) * epsilon
+
+    z = Lambda(sampling, output_shape=(LATENT_DIM,), name="z")([z_mean, z_log_var])
+
+    return Model(inputs, [z_mean, z_log_var, z], name="encoder")
+
+# Decoder
+def build_decoder():
+    latent_inputs = Input(shape=(LATENT_DIM,))
+
+    x = Dense(28 * 28 * 128, activation='relu')(latent_inputs)
+    x = Reshape((28, 28, 128))(x)
+
+    x = Conv2DTranspose(128, (3, 3), activation='relu', strides=2, padding='same')(x)
+    x = Conv2DTranspose(64, (3, 3), activation='relu', strides=2, padding='same')(x)
+    x = Conv2DTranspose(32, (3, 3), activation='relu', strides=2, padding='same')(x)
+    x = Conv2DTranspose(3, (3, 3), activation='sigmoid', padding='same')(x)
+
+    return Model(latent_inputs, x, name="decoder")
+
+# Sinkhorn Distance for EMD approximation
+def sinkhorn_distance(z_true, z_pred, epsilon=0.01, num_iters=50):
+    """
+    Approximate Earth Mover's Distance using the Sinkhorn-Knopp algorithm.
+    - z_true: (batch_size, latent_dim) - The latent representations from real samples.
+    - z_pred: (batch_size, latent_dim) - The latent representations from generated samples.
+    """
+    cost_matrix = tf.norm(tf.expand_dims(z_true, 1) - tf.expand_dims(z_pred, 0), axis=-1)
+
+    # Compute softmin for Sinkhorn iterations
+    u = tf.zeros_like(cost_matrix)
+    v = tf.zeros_like(cost_matrix)
+
+    for _ in range(num_iters):
+        u = -epsilon * tf.math.log(tf.reduce_mean(tf.exp(-cost_matrix / epsilon + v[:, None]), axis=1))
+        v = -epsilon * tf.math.log(tf.reduce_mean(tf.exp(-cost_matrix / epsilon + u[None, :]), axis=0))
+
+    sinkhorn_dist = tf.reduce_mean(cost_matrix * tf.exp(-cost_matrix / epsilon + u[:, None] + v[None, :]))
+    return sinkhorn_dist
+
+# Custom VAE model class with EMD loss
+class VAE(Model):
+    def __init__(self, encoder, decoder, **kwargs):
+        super(VAE, self).__init__(**kwargs)
+        self.encoder = encoder
+        self.decoder = decoder
+        self.mse_loss = tf.keras.losses.MeanSquaredError()
+
+    def call(self, inputs):
+        z_mean, z_log_var, z = self.encoder(inputs)
+        reconstructed = self.decoder(z)
+        return reconstructed
+
+    def train_step(self, data):
+        images, _ = data  # Only use images, labels are not needed
+        with tf.GradientTape() as tape:
+            z_mean, z_log_var, z = self.encoder(images)
+            reconstructed = self.decoder(z)
+
+            # Reconstruction Loss (MSE)
+            recon_loss = self.mse_loss(images, reconstructed)
+
+            # Earth Mover's Distance (Sinkhorn)
+            z_true = K.random_normal(shape=K.shape(z_mean))  # Sample from prior N(0,1)
+            emd_loss = sinkhorn_distance(z_true, z_mean)
+
+            # Total loss (Reconstruction + EMD)
+            total_loss = recon_loss + emd_loss
+
+        grads = tape.gradient(total_loss, self.trainable_variables)
+        self.optimizer.apply_gradients(zip(grads, self.trainable_variables))
+        return {"loss": total_loss, "reconstruction_loss": recon_loss, "emd_loss": emd_loss}
+
+# Function to create the complete VAE
+def create_vae():
+    encoder = build_encoder()
+    decoder = build_decoder()
+    vae = VAE(encoder, decoder)
+    return vae
